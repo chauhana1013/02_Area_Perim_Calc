1604cea7d89eaa4b2a2322184254be5ef358f145
chauhana1013 <100536433+chauhana1013@users.noreply.github.com>
2022-03-08 15:13:55 +1300

    Add files via upload

--- 01_looping_basic_v2.py
+++ 01_looping_basic_v2.py
@@ -1,14 +1,27 @@
+from ast import Try
+from multiprocessing.sharedctypes import Value
+from urllib import response
+
+
+# chesks  user has put a number more than zero
 valid = False
 while not valid:
 
+    error = "Please enter a number more than zero"
+    
     try:
             
-    #Ask user for n
-    response = float(input("Enter a number: "))
-    
-    if response > 0:
-        valid = True
+        # ask user for a number
+        response = float(input("Enter a number: "))
+        
+        # checks number is more than 0
+        if response > 0:
+            valid = True
 
-    else:
-        print("Please enter a number that is more than zero")
-        print()
+        #outputs error if input is invalid 
+        else:
+            print(error)
+            print()
+    
+    except ValueError: 
+        print(error)
