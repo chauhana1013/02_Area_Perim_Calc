e2281777bcee9d693ee77298fda4a04c70d5c444
chauhana1013 <100536433+chauhana1013@users.noreply.github.com>
2022-03-10 11:58:42 +1300

    Add files via upload

--- 01_looping_basic_v2.py
+++ 01_looping_basic_v2.py
@@ -1,27 +1,41 @@
-from ast import Try
-from multiprocessing.sharedctypes import Value
-from urllib import response
+# functions go here
 
+# checks number is more than zero
+def num_check(question):
+    valid = False
+    while not valid:
 
-# chesks  user has put a number more than zero
-valid = False
-while not valid:
-
-    error = "Please enter a number more than zero"
-    
-    try:
-            
-        # ask user for a number
-        response = float(input("Enter a number: "))
+        error = "Please enter a number more than zero"
         
-        # checks number is more than 0
-        if response > 0:
-            valid = True
+        try:
+                
+            # ask user for a number
+            response = float(input(question))
+            
+            # checks number is more than 0
+            if response > 0:
+                return response
 
-        #outputs error if input is invalid 
-        else:
+            #outputs error if input is invalid 
+            else:
+                print(error)
+                print()
+        
+        except ValueError: 
             print(error)
-            print()
-    
-    except ValueError: 
-        print(error)
+
+
+
+# Main Routine goes here 
+width = num_check("Width: ")
+height = num_check("Height: ")
+
+# Calculate Perimeter
+perimeter = (width * 2) + (height * 2)
+
+# Calculate Area
+area = width * height
+
+print()
+print("The Perimeter is {} units and the Area is {} squared units".format(perimeter, area))
+print()
